5971ed664a068f12b7706e6adb0b6aa9374c6525
AinurGeorge <gio.tatunashvili.oli@gmail.com>
2023-09-12 05:34:25 -0700

    additional images while scroll down, on the every different page that additional image is the same, also added grid section 2 image and 2 text

--- src/pages/ModelXPage.jsx
+++ src/pages/ModelXPage.jsx
@@ -1,5 +1,8 @@
 import modelXImage from '../assets/model-x.jpg'
+import ImageBackgroundNoText from '../components/ImageBackgroundNoText/ImageBackgroundNoText';
+import teslaBlueImage from '../assets/tesla-blue.jpg'
 import ImageBackgroundSection from '../components/ImageBackgroundSection/ImageBackgroundSection'
+import ImageGrid from '../components/ImageGrid/ImageGrid';
 
 const featureItems = [
     {title: '2.3s 0-60', description:'fast'},
@@ -17,6 +20,17 @@ export default function ModelXPage() {
                     description="View inventory"
                     backgroundImage={modelXImage}
                     featureItems={featureItems}
+                />
+            </section>
+            <section className='section'>
+                <ImageBackgroundNoText backgroundImage={teslaBlueImage}/>
+            </section>
+            <section className='section'>
+                <ImageGrid 
+                    image1={modelXImage}
+                    image2={teslaBlueImage}
+                    text1="Stay Connected - Instantly connect with multi-device Bluetooth, or fast charge devices with wireless and 36-watt USB-C charging."
+                    text2="Sublime Sound - A 22-speaker, 960-watt audio system with Active Road Noise Reduction offers the best listening experience wherever you are."
                 />
             </section>
         </div>
