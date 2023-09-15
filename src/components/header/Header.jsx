c090d158730a90d42f28229965ae7c4cbe6e875d
AinurGeorge <gio.tatunashvili.oli@gmail.com>
2023-09-15 07:45:37 -0700

    final updates, added shopPage and its functions with css, also added cartPage with the functional buttons. functional bugs fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,38 +1,58 @@
 import { useNavigate } from "react-router-dom";
 import './Header.css'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { useRecoilValue } from 'recoil'
+import { cartState } from '../../global-state/cartItems'
 
 export const navElements = [
     {label: 'TESLA', route: '', style: { marginRight: 'auto' } }, 
     {label: 'Model S', route: 'model_s'},
     {label: 'Model X', route: 'model_x'},
     {label: 'Cybertruck', route: 'cybertruck'},
-    {label: 'Menu', route: null,  style: {marginLeft: 'auto'} },
+    {label: 'Shop', route: 'shop'},
+    {label: <ShoppingCartIcon />, route: 'cart', style: {marginLeft: 'auto'} },
+    {label: 'Menu', route: null},
 ];
 
 export default function Header({setShowSidebar}) {
     const navigate = useNavigate()
+    const cartItems = useRecoilValue(cartState);
     return(
         <>
-            <nav>
-                <ul className="headerList">
-                    {navElements.map(({ label, route, style = {} }) => (
-                        <li
-                        style={style}
-                        key={label}
+          <nav>
+            <ul className="headerList">
+              {navElements.map(({ label, route, style = {} }) => {
+                  if (route === "cart") {
+                    return (
+                      <li 
                         onClick={() => {
-                          if (label === 'Menu') {
-                            setShowSidebar(true);
-                          } else {
-                            navigate(`/${route}`);
-                          }
+                            navigate(`/${route}`)
                         }}
+                        key={route}
+                        style={style}
                       >
+                        <span className="cart-items-count">{cartItems.length}</span>
                         {label}
                       </li>
-                    ))
-                }
+                    )
+                  }
+                return (
+                <li
+                  style={style}
+                  key={label}
+                  onClick={() => {
+                  if (label === 'Menu') {
+                    setShowSidebar(true);
+                  } else {
+                    navigate(`/${route}`);
+                    }
+                  }}
+                >
+                  {label}
+                </li>
+              )})}
             </ul>
-        </nav>
+          </nav>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,10 @@ import HomePage from './pages/HomePage.jsx'
 import ModelSPage from './pages/ModelSPage.jsx'
 import ModelXPage from './pages/ModelXPage.jsx'
 import CybertruckPage from './pages/CybertruckPage.jsx'
+import ShopPage from './pages/ShopPage.jsx'
+import CartPage from './pages/CartPage.jsx'
+import { RecoilRoot } from 'recoil';
+import { Toaster } from 'react-hot-toast'
 
 const router = createBrowserRouter([
   {
@@ -38,10 +42,27 @@ const router = createBrowserRouter([
         <CybertruckPage />
       </App>,
   },
+  {
+    path:'/shop',
+    element:
+      <App>
+        <ShopPage />
+      </App>,
+  },
+  {
+    path:'/cart',
+    element:
+      <App>
+        <CartPage />
+      </App>,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Toaster />
+    <RecoilRoot>
     <RouterProvider router={router} />
+    </RecoilRoot>
   </React.StrictMode>,
 )
